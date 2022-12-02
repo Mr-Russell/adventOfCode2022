2ca8844780f45450f881603a14fc06d015d4fa91
Brian Russell <brian_russell@hotmail.com>
2022-12-02 14:05:45 -0500

    Completed Day 2 Part 1

---
+++ Day2/script.js
@@ -0,0 +1,48 @@
+import { Console } from 'node:console';
+import { readFileSync } from 'node:fs';
+
+const strategy = readFileSync("./input.txt", "utf8").split("\n")
+
+const opponent = {
+    A: "rock",
+    B: "paper",
+    C: "scissors"
+}
+
+const me = {
+    X: ["rock", 1],
+    Y: ["paper", 2],
+    Z: ["scissors", 3]
+}
+
+const winners = {
+    "rock": "scissors",
+    "paper": "rock",
+    "scissors": "paper"
+}
+
+const stratPairs = strategy.map(pair=>pair.split(" "))
+
+let score = 0
+
+for (const pair of stratPairs){
+    const theirChoice = pair[0]
+    const myChoice = pair[1]
+    const myChoicePoints = me[myChoice][1]
+
+    score += myChoicePoints
+    
+    console.log("Opponent: ", opponent[theirChoice])
+    console.log("Me:     : ", me[myChoice][0])
+
+    if (opponent[theirChoice] === me[myChoice][0]){
+        score += 3
+    } else if (opponent[theirChoice] === winners[me[myChoice][0]]){
+        score += 6
+        console.log(`${me[myChoice][0]} BEATS ${opponent[theirChoice]}!`)
+    }
+
+}
+
+//Part 1 Answer
+console.log("Part 1 Answer: ", score)
