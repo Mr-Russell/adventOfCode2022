87d1d5a00d051d203f0e77ddfa58ce006da01a6b
Brian Russell <brian_russell@hotmail.com>
2022-12-04 15:15:40 -0500

    Completed Day 4 Part 2

--- Day4/part2.js
+++ Day4/part2.js
@@ -8,10 +8,14 @@ for (let i = 0; i < sections.length; i += 2){
     //Without parseInt, code produces wrong answer due to type-coercion 
     const elf1 = sections[i].split("-").map(num=>parseInt(num))
     const elf2 = sections[i+1].split("-").map(num=>parseInt(num))
-
-   if ((elf1[0] <= elf2[0] && elf1[1] >= elf2[1]) || (elf2[0] <= elf1[0] && elf2[1] >= elf1[1])){
+    
+    // If elf 1's Max is Less Than elf 2's Min
+    // Or
+    // If elf1's Min is Greater Than elf 2's Max
+    // Then neither can possibly overlap
+    if (!(elf1[1] < elf2[0] || elf1[0] > elf2[1])){
     overlaps++
-   }
+    }
 }
 
 console.log(overlaps)
