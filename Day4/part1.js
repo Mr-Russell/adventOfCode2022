90f235018185b823315c58c5501f1542ecfcfc30
Brian Russell <brian_russell@hotmail.com>
2022-12-04 14:05:07 -0500

    Completed Day 4 Part 1

---
+++ Day4/part1.js
@@ -0,0 +1,18 @@
+import { readFileSync } from 'node:fs';
+
+const sections = readFileSync("./input.txt", "utf8").split(/[\n,]/)
+
+let overlaps = 0
+
+for (let i = 0; i < sections.length; i += 2){
+    //Without parseInt, code produces wrong answer due to type-coercion 
+    const elf1 = sections[i].split("-").map(num=>parseInt(num))
+    const elf2 = sections[i+1].split("-").map(num=>parseInt(num))
+
+   if (
+    (elf1[0] <= elf2[0] && elf1[1] >= elf2[1]) || (elf2[0] <= elf1[0] && elf2[1] >= elf1[1])){
+    overlaps++
+   }
+}
+
+console.log(overlaps)
